f9b4fdff375e15761d4154ea78bcd28ddbd507f4
Victor Mello <victor.morenomello@gmail.com>
2022-01-09 22:10:59 -0300

    Aula 7 - Inclusão de nível e pontuação

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -5,10 +5,22 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
-print(numero_secreto)
 total_de_tentativas = 3
-rodada = 1
+pontos = 1000
 
+### Definição de nível de dificuldade
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível que deseja jogar:"))
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+### Laço de repetição
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite um número entre 1 e 100: ")
@@ -18,19 +30,20 @@ for rodada in range(1, total_de_tentativas + 1):
         print("Você deve digitar um número entre 1 e 100!")
         continue
 
-
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou")
+        print("Você acertou e fez {} pontos".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! Seu chute foi maior que o número secreto!")
         elif(menor):
             print("Você errou! Seu chute foi menor que o número secreto!")
+        pontosperdidos = numero_secreto - chute
+        pontos = pontos - abs(pontosperdidos)
 
 
 print("Fim do jogo")
